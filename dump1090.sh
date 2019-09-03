fce5db24973b1a0df8a95f3936c66831c6443791
Patrick Socha <patrick.socha@inflightdublin.com>
2019-09-03 10:03:46 +0100

    Few improvements

--- dump1090.sh
+++ dump1090.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 DUMPDIR=/data/flight/dump1090
-DATADIR=$DUMPDIR/data
-tar cfz $DUMPDIR/data_$(date +%Y%m%d_%H%M%S).tar.gz $DATADIR
+DATADIR=data
+pushd $DUMPDIR
+tar cfz data_$(date +%Y%m%d_%H%M%S).tar.gz $DATADIR
 rm -f $DATADIR/*
 $DUMPDIR/dump1090 --fix --quiet --gain 40 --write-json $DATADIR --write-mongo-uri "mongodb://localhost" --write-mongo-db "flightdata" --write-mongo-col "aircraftdata"
+popd
